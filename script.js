55abae5e3a0b23f21009ec7dd66e2dd2e762a9d8
OmKadam-1 <kom110798@gmail.com>
2025-09-07 21:59:42 +0530

    js file completed

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("");
+const tasks = [];
+
+function LocalStorage() {
+    const Tasks = JSON.parse(localStorage.getItem("Tasks"));
+
+    if (Tasks) {
+        tasks.push(...Tasks);
+    }
+
+    loadTasks();
+}
+LocalStorage();
+
+function loadTasks() {
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+
+    const tasksContainer = document.getElementById("tasks-container");
+    tasksContainer.innerHTML = "";
+
+    for (const task of tasks) {
+        tasksContainer.innerHTML += `
+        <div class="todo-item">
+          ${task}
+          <button class="btn-del" type="button" onclick="deleteTask('${task}')">
+            Delete
+          </button>
+        </div>
+        `;
+    }
+}
+
+function deleteTask(task) {
+    const taskIndex = tasks.indexOf(task);
+    tasks.splice(taskIndex, 1);
+
+    loadTasks();
+}
+
+function addTask() {
+    const taskInputElement = document.getElementById("task-input");
+    const task = taskInputElement.value;
+
+    if (!task) {
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.unshift(task);
+    loadTasks();
+
+    taskInputElement.value = "";
+}
